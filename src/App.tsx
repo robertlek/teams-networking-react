1224236af5c60c562b7f6ac3f3e08b9afc72a328
robertlek <lecarobertandrei@gmail.com>
2023-05-10 18:10:21 +0300

    Cleaned up the old React App code.

--- src/App.tsx
+++ src/App.tsx
@@ -153,20 +153,4 @@ function App() {
     );
 }
 
-function App_old() {
-    return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.tsx</code> and save to reload.
-                </p>
-                <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-                    Learn React
-                </a>
-            </header>
-        </div>
-    );
-}
-
 export default App;
