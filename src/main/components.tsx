c304bfcacff2548d7c90f33c78532f710c4dc29a
robertlek <lecarobertandrei@gmail.com>
2023-05-22 21:44:12 +0300

    Added a search input that uses state hook.

--- src/main/components.tsx
+++ src/main/components.tsx
@@ -1,7 +1,10 @@
+import { useState } from "react";
 import { SideMenu } from "../menu/SideMenu";
 import { TeamsTableWrapper } from "../teams/TeamsTable";
 
 export function ContentWrapper() {
+    const [search, setSearch] = useState("JS");
+
     return (
         <section id="container">
             <SideMenu />
@@ -9,6 +12,15 @@ export function ContentWrapper() {
                 <div className="display-content" id="home"></div>
                 <div className="display-content" id="skills"></div>
                 <div className="display-content" id="teams" style={{ display: "block" }}>
+                    <input
+                        type="search"
+                        placeholder="Search"
+                        value={search}
+                        onChange={e => {
+                            setSearch(e.target.value);
+                        }}
+                    />
+                    <span>🔎 searching {search}</span>
                     <TeamsTableWrapper />
                 </div>
                 <div className="display-content" id="languages"></div>
